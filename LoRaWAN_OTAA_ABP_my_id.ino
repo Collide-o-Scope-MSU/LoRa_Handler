9c1fe9556ad3187e7d2f2ea7a80443432308ce60
ajaya.dahal <ajayadahal1000@gmail.com>
2022-04-21 23:48:44 -0500

    Code before adding GPS function

--- LoRaWAN_OTAA_ABP_my_id.ino
+++ LoRaWAN_OTAA_ABP_my_id.ino
@@ -1,24 +1,3 @@
-/**
- * @file LoRaWAN_OTAA_ABP.ino
- * @author rakwireless.com
- * @brief LoRaWan node example with OTAA/ABP registration
- * @version 0.1
- * @date 2020-08-21
- * 
- * @copyright Copyright (c) 2020
- * 
- * @note RAK4631 GPIO mapping to nRF52840 GPIO ports
-   RAK4631    <->  nRF52840
-   WB_IO1     <->  P0.17 (GPIO 17)
-   WB_IO2     <->  P1.02 (GPIO 34)
-   WB_IO3     <->  P0.21 (GPIO 21)
-   WB_IO4     <->  P0.04 (GPIO 4)
-   WB_IO5     <->  P0.09 (GPIO 9)
-   WB_IO6     <->  P0.10 (GPIO 10)
-   WB_SW1     <->  P0.01 (GPIO 1)
-   WB_A0      <->  P0.04/AIN2 (AnalogIn A2)
-   WB_A1      <->  P0.31/AIN7 (AnalogIn A7)
- */
 #include <Arduino.h>
 #include <LoRaWan-RAK4630.h> //http://librarymanager/All#SX126x
 #include <SPI.h>
@@ -44,11 +23,11 @@ bool doOTAA = true;   // OTAA is used by default.
 #define BG77_GPS_ENABLE WB_IO2
 
 DeviceClass_t g_CurrentClass = CLASS_A;					/* class definition*/
-LoRaMacRegion_t g_CurrentRegion = LORAMAC_REGION_US915;    /* Region:EU868*/
+LoRaMacRegion_t g_CurrentRegion = LORAMAC_REGION_US915;    /* Region:US915*/
 lmh_confirm g_CurrentConfirm = LMH_CONFIRMED_MSG;				  /* confirm/unconfirm packet definition*/
 uint8_t gAppPort = LORAWAN_APP_PORT;							        /* data port*/
 
-char rspchar[256] = {0}; //add this at the top of your program
+char rspchar[256] = {0}; 
 
 /**@brief Structure containing LoRaWan parameters, needed for lmh_init()
 */
@@ -67,6 +46,7 @@ static lmh_callback_t g_lora_callbacks = {BoardGetBatteryLevel, BoardGetUniqueId
                                         lorawan_rx_handler, lorawan_has_joined_handler, lorawan_confirm_class_handler, lorawan_join_failed_handler
                                        };
 //OTAA keys !!!! KEYS ARE MSB !!!!
+//AD4630
 uint8_t nodeDeviceEUI[8] = {0x60, 0x81, 0xF9, 0x1A, 0x00, 0xD8, 0x9D, 0x17};
 uint8_t nodeAppEUI[8] = {0x60, 0x81, 0xF9, 0x2E, 0xEA, 0xBC, 0x1A, 0x0B};
 uint8_t nodeAppKey[16] = {0xA7, 0x6E, 0x8F, 0x9B, 0xC9, 0x77, 0xEA, 0x76, 0x11, 0x02, 0xB8, 0x15, 0xD0, 0x94, 0x21, 0x75};
@@ -82,17 +62,14 @@ uint8_t nodeAppsKey[16] = {0xFB, 0xAC, 0xB6, 0x47, 0xF3, 0x58, 0x45, 0xC7, 0x50,
 static uint8_t m_lora_app_data_buffer[LORAWAN_APP_DATA_BUFF_SIZE];            //< Lora user application data buffer.
 static lmh_app_data_t m_lora_app_data = {m_lora_app_data_buffer, 0, 0, 0, 0}; //< Lora user application data structure.
 
-TimerEvent_t appTimer;
-static uint32_t timers_init(void);
+
 static uint32_t count = 0;
 static uint32_t count_fail = 0;
 float battery =0.0;
 bool hasSent = false;
 uint8_t crash_status = 0;
-
 float x,y,z = 0.00;
 LIS3DH SensorTwo(I2C_MODE, 0x18);
-
 
 
 float getBatteryLife( ){
@@ -100,40 +77,15 @@ float getBatteryLife( ){
   memset(batteryLife,0,64);
   memset(rspchar, 0, 256);
   bg77_at("AT+CBC",2000);
-    bg77_at("AT+CBC",2000);
-  
-    char *p;
-    char * res[256] = {0};
-    
-    p = strtok(rspchar, ",");
-
- 
-
-    if(p)
-    {
-      //strncpy(n, p, sizeof(p));
-    //printf("North: %s\n", n);
-    }
-    p = strtok(NULL, ",");
-
- 
-
-    if(p){
-            //strncpy(w, p, sizeof(p));
-           //printf("West: %s\n", w);
-           }
-        p = strtok(NULL, ",");
-
- 
-
-    if(p){
-            strncpy(batteryLife, p, sizeof(p));
-           //printf("West: %s\n", w);
-           }
-
- 
-
-
+  bg77_at("AT+CBC",2000);
+  char *p;
+  char * res[256] = {0};
+  p = strtok(rspchar, ",");
+  p = strtok(NULL, ",");
+  p = strtok(NULL, ",");
+  if(p){
+    strncpy(batteryLife, p, sizeof(p));
+  }
   Serial.printf("Battery life in mv: %s\n", batteryLife);
   float f = atof(batteryLife);
   Serial.printf("Battery life float: %.3f\n", f);
@@ -141,12 +93,10 @@ float getBatteryLife( ){
   memset(batteryLife,0,64);
   sprintf(batteryLife, "%.3f", f);
   Serial.printf("Battery life string in V: %s\n", batteryLife);
+  return f;  
+}
 
-  return f;
-  }
-
-void bg77_at(char *at, uint16_t timeout)
-{
+void bg77_at(char *at, uint16_t timeout){
   String bg77_rsp="";
   char tmp[256] = {0};
   int len = strlen(at);
@@ -165,8 +115,6 @@ void bg77_at(char *at, uint16_t timeout)
   }
   Serial.println(bg77_rsp);
   strncpy(rspchar, bg77_rsp.c_str(), bg77_rsp.length());
-  //Serial.printf("Response char array: %s", rspchar);
-  //memset(rspchar,0,256);
   bg77_rsp = "";
 }
 
@@ -205,30 +153,13 @@ void setup_bg77(){
   bg77_at("AT+QGMR", 2000);
   //active and join to the net, this part may depend on some information of your operator.
   bg77_at("AT+CFUN=1,0", 500);
-  //delay(2000);
   bg77_at("AT+CPIN?", 500);
-  //delay(2000);
-  //bg77_at("AT+COPS=?", 2000);
-
-//delay(60000);
-  
-  //bg77_at("AT+QNWINFO", 500);
-  //delay(2000);
-  //bg77_at("AT+QCSQ", 500);
- // delay(2000);
-  //bg77_at("AT+CSQ", 500);
-  //delay(2000);
-  //bg77_at("AT+QICSGP=1,1,\"hologram\",\"\",\"\",1", 2000);
   bg77_at("AT+QIDEACT=1", 3000);
   bg77_at("AT+QICSGP=1,1,\"hologram\",\"\",\"\",0",2000);
-  //delay(2000);
   bg77_at("AT+QIACT=1", 3000);
-
   //open tcp link with Hologram server
   bg77_at("AT+QIOPEN=1,0,\"TCP\",\"cloudsocket.hologram.io\",9999,0,1", 5000);
-  
   delay(2000);
-  
 }
 void bg77_shutdown(){ 
   bg77_at("AT+QCFG=\"fast/poweroff\",63,1", 2000);
@@ -258,12 +189,8 @@ void setup()
   {
     Serial.println("Sensor at 0x18 started.");
   }
-  Serial.println("enter !");
-
-
   // Initialize LoRa chip.
   lora_rak4630_init();
-
   // Initialize Serial for debug output
   time_t timeout = millis();
   Serial.begin(115200);
@@ -281,6 +208,8 @@ void setup()
   setup_bg77();
   delay(10);
   battery = getBatteryLife();
+  Serial.print("Battery: ");
+  Serial.println(battery);
   bg77_shutdown();
   
   Serial.println("=====================================");
@@ -293,7 +222,6 @@ void setup()
   {
     Serial.println("Type: ABP");
   }
-
   switch (g_CurrentRegion)
   {
     case LORAMAC_REGION_AS923:
@@ -323,15 +251,6 @@ void setup()
   }
   Serial.println("=====================================");
   
-  //creat a user timer to send data to server period
-  uint32_t err_code;
-  err_code = timers_init();
-  if (err_code != 0)
-  {
-    Serial.printf("timers_init failed - %d\n", err_code);
-    return;
-  }
-
   // Setup the EUIs and Keys
   if (doOTAA)
   {
@@ -345,7 +264,6 @@ void setup()
     lmh_setAppSKey(nodeAppsKey);
     lmh_setDevAddr(nodeDevAddr);
   }
-
   // Initialize LoRaWan
   err_code = lmh_init(&g_lora_callbacks, g_lora_param_init, doOTAA, g_CurrentClass, g_CurrentRegion);
   if (err_code != 0)
@@ -353,7 +271,6 @@ void setup()
     Serial.printf("lmh_init failed - %d\n", err_code);
     return;
   }
-
   // Start Join procedure
   lmh_join();
 }
@@ -372,6 +289,9 @@ void loop()
     Serial.println(sqrt(sq(x) + sq(y) + sq(z)));
     Serial.println("Sending frame now...");
     send_lora_frame();
+    // Start Join procedure
+    lmh_join();
+    delay(2000);
     
    }
    
@@ -383,7 +303,6 @@ void loop()
 void lorawan_has_joined_handler(void)
 {
   Serial.println("OTAA Mode, Network Joined!");
-
   lmh_error_status ret = lmh_class_request(g_CurrentClass);
   if (ret == LMH_SUCCESS)
   {
@@ -430,13 +349,6 @@ void send_lora_frame(void)
   uint32_t i = 0;
   memset(m_lora_app_data.buffer, 0, LORAWAN_APP_DATA_BUFF_SIZE);
   m_lora_app_data.port = gAppPort;
-//  m_lora_app_data.buffer[i++] = 'A';
-//  m_lora_app_data.buffer[i++] = 'J';
-//  m_lora_app_data.buffer[i++] = 'A';
-//  m_lora_app_data.buffer[i++] = 'Y';
-//  m_lora_app_data.buffer[i++] = 'A';
-//  m_lora_app_data.buffer[i++] = '!';
-
   m_lora_app_data.buffer[i++] = 0x09;    
   m_lora_app_data.buffer[i++] = (battery/4.8)*100; //& 0xFF000000) >> 24;
   m_lora_app_data.buffer[i++] = crash_status;//(battery & 0x00FF0000) >> 16;
@@ -457,24 +369,4 @@ void send_lora_frame(void)
     count_fail++;
     Serial.printf("lmh_send fail count %d\n", count_fail);
   }
-}
-
-/**@brief Function for handling user timerout event.
- */
-void tx_lora_periodic_handler(void)
-{
-  TimerSetValue(&appTimer, LORAWAN_APP_INTERVAL);
-  TimerStart(&appTimer);
- 
-  //send_lora_frame();
-}
-
-/**@brief Function for the Timer initialization.
- *
- * @details Initializes the timer module. This creates and starts application timers.
- */
-uint32_t timers_init(void)
-{
-  TimerInit(&appTimer, tx_lora_periodic_handler);
-  return 0;
 }
